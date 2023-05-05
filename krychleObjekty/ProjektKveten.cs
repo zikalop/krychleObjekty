d9fa56744a794623aeb122d733d52d9b2ca5faa0
zikalop <vojtazika2@gmail.com>
2023-05-05 12:01:41 +0200

    projektKveten

---
+++ krychleObjekty/ProjektKveten.cs
@@ -0,0 +1,19 @@
+﻿namespace projektKveten
+{
+    public class all
+    {
+        public static class Product
+        {
+            public static string name { get; set; }
+            public static double price { get; set; }
+
+        }
+        public static class Inventory
+        {
+            public static string products;
+            public static string counts;
+            public static string honvo;
+        }
+
+    }
+}
