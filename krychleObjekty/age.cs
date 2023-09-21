250bfd2e2e6d1f04e0533981729cab7f7539acd3
zikalop <vojtazika2@gmail.com>
2023-09-21 10:14:00 +0200

    generace hotovo

--- krychleObjekty/age.cs
+++ krychleObjekty/age.cs
@@ -4,24 +4,43 @@
     {
         public class neznamy
         {
-            public static string otz;
+            public static string otazka;
             public static int vek;
 
         }
 
         public static void Main()
         {
-            
-            Console.WriteLine("Kdy ses narodil (napiš pouze rok)");
-            neznamy.vek= int.Parse(Console.ReadLine());
-            switch (neznamy.vek)
+            try
             {
-                case neznamy.vek >= 1925 and neznamy.vek <= 1941:
-                    Console.WriteLine("Jsi generace silent");
-                    break;
+                Console.WriteLine("Kdy ses narodil (napiš pouze rok)");
+                neznamy.vek = int.Parse(Console.ReadLine());
+                if (neznamy.vek >= 1925 && neznamy.vek <= 1941)
+                {
+                    Console.WriteLine("Jsi z generace Silent");
+                }
+                if (neznamy.vek >= 1946 && neznamy.vek <= 1964)
+                {
+                    Console.WriteLine("Jsi z generace Baby boomers");
+                }
+                if (neznamy.vek >= 1964 && neznamy.vek <= 1979)
+                {
+                    Console.WriteLine("Jsi z generace X ");
+                }
+                if (neznamy.vek >= 1980 && neznamy.vek <= 1997)
+                {
+                    Console.WriteLine("Jsi z generace Y");
+                }
+                if (neznamy.vek >= 1997 && neznamy.vek <= 2023)
+                {
+                    Console.WriteLine("Jsi z generace Z ");
+                }
             }
-
+            catch (Exception)
+            {
+                Console.WriteLine("Špatný input");
+            }
         }
-    }
 
+    }
 }
