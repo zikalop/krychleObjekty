6ba361fbabc19633d280de83b6f383dee1c80730
zikalop <vojtazika2@gmail.com>
2023-11-09 12:56:00 +0100

    hotovo

--- krychleObjekty/TokiPona.cs
+++ krychleObjekty/TokiPona.cs
@@ -4,8 +4,7 @@ namespace TokiPona
 {
     public static class all
     {
-
-        public static void Main()
+        public static void tokiP()
         {
             AnsiConsole.MarkupLine(@"[green]        ,----,                                                                                 
       ,/   .`|                                   ,-.----.                                      
@@ -24,8 +23,45 @@ namespace TokiPona
                                  ---`-'            `---`                          `--`---'     
                                                                                                [/]");
             Console.WriteLine("-------------------------------------------------------------------------------------------------------------------");
-            Dictionary<string,string> slovnik_toki= new Dictionary<string,string>();
-            
+        }
+        
+        public static void Main()
+        {
+            Dictionary<string, string> slovnik_toki = new Dictionary<string, string>();
+            bool cycle = true;
+            tokiP();
+            while (cycle= true)
+            {               
+                Console.WriteLine("zadej 1. věc a pak 2.(při třetim stisknutí je mozno zmáčknout k jako konec)");
+                string input1 = Console.ReadLine();
+                string input2 = Console.ReadLine();
+                ConsoleKeyInfo input3 = Console.ReadKey();
+                char inputC='i';
+                slovnik_toki.Add(input1, input2);
+                switch(inputC)
+                {
+                    case 'k' or 'K':
+                        break;
+                }
+                
+                Console.Clear();
+                tokiP();
+            }
+            var tabule = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("Vyber si:")
+                    .PageSize(10)
+                    .MoreChoicesText("")
+                    .AddChoices("Zobrazit vyplněné hodnoty"));            
+            switch (tabule)
+            {
+                case "Zobrazit vyplněné hodnoty":
+                    foreach (KeyValuePair<string, string> s in slovnik_toki)
+                    {
+                        Console.WriteLine(s);
+                    }
+                    break;
+            }
         }
     }
 }
