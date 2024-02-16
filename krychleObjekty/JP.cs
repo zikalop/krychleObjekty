3e2c8e57f0c73e838162a2627cc68fbf5964842c
zikalop <125461310+zikalop@users.noreply.github.com>
2024-02-16 07:32:34 +0100

    kamo nevim kdy to dodělam

---
+++ krychleObjekty/JP.cs
@@ -0,0 +1,47 @@
+﻿using Spectre.Console;
+
+namespace JP
+{
+    public class all
+    {
+        public class obj
+        {
+
+        }
+
+        public static void Main()
+        {
+            var tabule = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("Co chceš vybrat?")
+                    .PageSize(10)
+                    .MoreChoicesText("")
+                    .AddChoices("Slovník","Časové údaje", "Slovesa", "Pozdravy", "První setkání"));
+
+        }
+        public static void slovník()
+        {
+
+        }
+        
+        public static void časU()
+        {
+            string[] čSlova = {"Ototoi",};
+        }
+
+        public static void slovesa()
+        {
+
+        }
+
+        public static void pozdravy()
+        {
+
+        }
+
+        public static void prvníS()
+        {
+
+        }
+    }
+}
